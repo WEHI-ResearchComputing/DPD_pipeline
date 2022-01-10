a23c96cb6204a571be5428cde642806299448bb5
ChrisBollands <cgbollands@gmail.com>
2022-01-10 14:12:44 +1100

    dropped files from git ignore

--- aspera_tests/subprocess_test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-process = subprocess.run(["python3", '/home/users/allstaff/bollands.c/scratch/bollands.c/Malaria_downloads/enaBrowserTools-0.0.3/enaBrowserTools-0.0.3/python3/enaDataGet.py', "-a", "-m", "ERR1081237"], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-
-print(process.stderr)
-
-# if process.stderr != None:
-#     print(process.stderr)
