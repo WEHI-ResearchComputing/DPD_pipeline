b73cb9353601e4b1aff1366133053961e3e2365b
ChrisBollands <cgbollands@gmail.com>
2022-03-18 16:12:53 +1100

    fixed script scripts

--- scripts_script.sh
+++ scripts_script.sh
@@ -1,2 +1,12 @@
-source ./hello_world.sh Chris
-source ./hello_world.sh Chris
+#!/bin/bash
+#SBATCH --job-name=singlecore_job
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=4
+#SBATCH --mem=1G
+#SBATCH --output=scripts_script.out
+
+echo $1
+
+source ../../DPD_pipeline/jocelyn_scripts/fastq2bamVivax.sh $1
+source ../../DPD_pipeline/jocelyn_scripts/gatk_haplocallVivax.sh $1
+source ../../DPD_pipeline/jocelyn_scripts/copynumVivax.sh $1
